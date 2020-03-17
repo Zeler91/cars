be67328f9c5897d1bd5eafe1bbfa3abfc8bcf1f4
Zeler91 <51795219+Zeler91@users.noreply.github.com>
2020-03-17 20:57:13 +0300

    Update settings.py

--- cars/settings.py
+++ cars/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!0e!ct-=gy1g%l*o&3yc!cdp73#@pt+f72cc9d882gh_o0p5)z'
-
+#SECRET_KEY = '!0e!ct-=gy1g%l*o&3yc!cdp73#@pt+f72cc9d882gh_o0p5)z'
+SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -78,6 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'HOST': os.getenv("DB_HOST", "127.0.0.1"),
+        'PORT': os.getenv("DB_PORT", 5432),
     }
 }
 
